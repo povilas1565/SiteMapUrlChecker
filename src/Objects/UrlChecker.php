aa13dcf810b1674048b532293d6aa231e5f4bbe5
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-07-24 16:19:06 +0300

    SiteChecker

---
+++ src/Objects/UrlChecker.php
@@ -0,0 +1,68 @@
+<?php /** @noinspection PhpUnnecessaryLocalVariableInspection */
+
+namespace RapidWeb\SiteMapUrlChecker\Objects;
+
+use RapidWeb\uxdm\Objects\Sources\CSVSource;
+use RapidWeb\uxdm\Objects\Sources\XMLSource;
+use RapidWeb\uxdm\Objects\Destinations\CSVDestination;
+use RapidWeb\uxdm\Objects\Migrator;
+use RapidWeb\uxdm\Objects\DataItem;
+use GuzzleHttp\Client;
+
+abstract class UrlChecker
+{
+    private static $sourceFile;
+    private static $destinationFile;
+
+    private static function checkUrl($url){
+        $client = new Client(['exceptions'=> false]);
+
+        $response = $client->get($url);
+
+        $urlStatusText = $response->getReasonPhrase();
+        $urlStatusCode = $response->getStatusCode();
+
+        $urlStatus = $urlStatusText."|".$urlStatusCode;
+        return $urlStatus;
+
+
+
+    }
+    private static function migrate($sourceField,$destinationField)
+    {
+        $migrator = new Migrator;
+
+        $migrator->setSource(self::$sourceFile)
+            ->setDestination(self::$destinationFile)
+            ->setFieldsToMigrate([$sourceField])
+            ->setDataRowManipulator(function($dataRow) use ($sourceField,$destinationField){
+                $url =  $dataRow->getDataItemByFieldName($sourceField);
+
+                $urlResult = Self::checkUrl($url->value);
+                $dataRow->addDataItem(new DataItem($destinationField,$urlResult, true));
+
+            })
+            ->migrate();
+    }
+
+    public static function checkUrlListFromCsv($sourceFilename,$sourceField,$destinationCsvFileName,$destinationField){
+        self::$sourceFile = new CsvSource($sourceFilename);
+        self::$destinationFile = new csvDestination($destinationCsvFileName);
+
+        Self::migrate($sourceField,$destinationField);
+
+    }
+
+    /** @noinspection PhpUnused */
+    public static function checkUrlFromXmlSiteMap($sourceFilename, $destinationCsvFileName, $destinationField)
+    {
+        self::$sourceFile = new XMLSource($sourceFilename, '/ns:urlset/ns:url');
+        self::$sourceFile->addXMLNamespace('ns', 'https://www.sitemaps.org/schemas/sitemap/0.9');
+        self::$destinationFile = new csvDestination($destinationCsvFileName);
+
+        Self::migrate('loc',$destinationField);
+
+
+    }
+
+}
